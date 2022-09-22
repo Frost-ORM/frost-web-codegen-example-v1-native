346e8fa4f6d0463de6d9637dbfac97c162c1941d
samishal1998 <samishal.1998@gmail.com>
2022-09-22 21:33:00 +0200

    First Commit

--- src/database/frost.ts
+++ src/database/frost.ts
@@ -6,14 +6,6 @@ getDatabase
 } from "@firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC-a69VedItFUk2K_9wUyINaQRF_XzJy7M",
-    authDomain: "contractors-f1d51.firebaseapp.com",
-    databaseURL: "https://contractors-f1d51-default-rtdb.firebaseio.com",
-    projectId: "contractors-f1d51",
-    storageBucket: "contractors-f1d51.appspot.com",
-    messagingSenderId: "765929125502",
-    appId: "1:765929125502:web:ed8a7e28fd7751466b8a2f",
-    measurementId: "G-E1Z90RWJ2Z"
   }
 
 if(!Object.keys(firebaseConfig).length) {
